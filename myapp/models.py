21cfe1d7956c834ae5d75913c4b994ee484987b5
tromeel <ntululu2016@gmail.com>
2025-07-01 11:42:40 +0300

    initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.CharField(max_length=20)
+    medical_history =models.TextField()
+    dob = models.DateField()
+
+    def __str__(self):
+         return self.firstname + " " + self.lastname
+
+class Doctor(models.Model):
+    fullname = models.CharField(max_length=100)
+    doctorId = models.IntegerField
+    age =models.IntegerField()
+    department= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.fullname
+
+class Ward(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    department = models.CharField(max_length=100)
+    floor = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('', views.index, name='index'),
     path('starter/', views.starter, name='starter'),
     path('about/', views.about, name='about'),
